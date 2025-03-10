548b321ed74645a7027dc234960b673bf8135217
Mohd Tauseef <88592795+tauseefm229@users.noreply.github.com>
2025-03-11 03:54:42 +0530

    Update DatePrep_VSCode_py_file.py

--- DatePrep_VSCode_py_file.py
+++ DatePrep_VSCode_py_file.py
@@ -1,22 +1,22 @@
 import pandas as pd
 
 # !pip install --upgrade pip    ----> running in terminal
-# !pip install dataprep
+# !pip install dataprep         ----> running in terminal
 
 from dataprep.eda import create_report
 
 # Example dataset (replace with your actual DataFrame)
 df = pd.read_csv("Spotify_data.csv")
 
-# report = create_report(df)
-# report.show()  
-# report.save("data_prep_report.html")
+report = create_report(df)
+report.show()  
+report.save("data_prep_report.html")
 
 # Generate EDA report
-# report = create_report(df)
-# report.save("eda_report.html")  # Report has been saved to eda_report.html!
+report = create_report(df)
+report.save("eda_report.html")  # Report has been saved to eda_report.html!
 
-# create_report(df).show_browser()
+create_report(df).show_browser()
 
 
 
